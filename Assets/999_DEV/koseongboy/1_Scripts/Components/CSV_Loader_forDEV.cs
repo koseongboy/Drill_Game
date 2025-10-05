591ff0180c7b06fe00bf59a95e360bdf14c2ced6
고성현 <80733733+koseongboy@users.noreply.github.com>
2025-10-05 19:30:43 +0900

    CSV 데이터 구조 변경
    기존 x, y 따로 컬럼 나누던 것을 한 칸 안에 여러 좌표를 넣는 것으로 변경

--- Assets/999_DEV/koseongboy/1_Scripts/Components/CSV_Loader_forDEV.cs
+++ Assets/999_DEV/koseongboy/1_Scripts/Components/CSV_Loader_forDEV.cs
@@ -9,9 +9,24 @@ namespace DrillGame.Components
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         async void Start()
         {
+
             Engine_Data engineData = await Engine_Data.CreateAsync();
-            var coords = engineData.GetCoordinate("A-3");
-            foreach(var coord in coords)
+            var coords = engineData.GetCoordinate("normal-3");
+            foreach (var coord in coords)
+            {
+                Debug.Log($"Coordinate: ({coord.Item1}, {coord.Item2})");
+            }
+            var coords2 = engineData.GetCoordinate("special-2");
+            foreach (var coord in coords2)
+            {
+                Debug.Log($"Coordinate: ({coord.Item1}, {coord.Item2})");
+            }
+
+
+            Facility_Data facilityData = await Facility_Data.CreateAsync();
+            var facilityStruct = facilityData.GetFacility_Structure("iron-1");
+            Debug.Log($"Facility ID: {facilityStruct.id}, Name: {facilityStruct.name}");
+            foreach (var coord in facilityStruct.coordinates)
             {
                 Debug.Log($"Coordinate: ({coord.Item1}, {coord.Item2})");
             }
