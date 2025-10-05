1657707d62a8a287a0b2c8da1763553ec84eb9fe
고성현 <80733733+koseongboy@users.noreply.github.com>
2025-10-05 19:46:16 +0900

    CSV 로더 추가

---
+++ Assets/1_Play/1_Scripts/Managers/CSVLoader.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using DrillGame.Data;
+using System;
+using System.Collections.Generic;
+
+
+namespace DrillGame.Managers
+{
+    public class CSVLoader : MonoBehaviour
+    {
+        #region Fields & Properties
+            private Engine_Data engineData;
+            private Facility_Data facilityData;
+        #endregion
+
+        #region Singleton & initialization
+        #endregion
+
+        #region getters & setters
+            public Engine_Data GetEngineData() { return engineData; }
+            public Facility_Data GetFacilityData() { return facilityData; }
+            public List<Tuple<int, int>> GetEngineCoordinates(string id) { return engineData.GetCoordinate(id); }
+            public Facility_Structure GetFacilityStructure(string id) { return facilityData.GetFacility_Structure(id); }
+        #endregion
+
+        #region public methods
+        #endregion
+
+        #region private methods
+        #endregion
+
+        #region Unity event methods
+        async void Awake()
+        {
+            engineData = await Engine_Data.CreateAsync();
+            facilityData = await Facility_Data.CreateAsync();
+        }
+
+        
+        void Update()
+        {
+        
+        }
+        #endregion
+
+    }
+}
