659aa9d6bc7c6554125267307e6c86ff1ed40bbd
고성현 <80733733+koseongboy@users.noreply.github.com>
2025-10-06 21:26:54 +0900

    CSV로더 삭제(로직엔 문제 x), 땅 갈아엎기

--- Assets/999_DEV/koseongboy/1_Scripts/Managers/DataLoadedChecker.cs
+++ Assets/999_DEV/koseongboy/1_Scripts/Managers/DataLoadedChecker.cs
@@ -20,26 +20,29 @@ namespace DrillGame
         #endregion
 
         #region private methods
+        IEnumerator DataCheckCoroutine()
+        {
+            yield return new WaitForSeconds(2.0f); //1초 대기
+            var engineTable = DataLoadManager.Instance.GetEngineTable();
+            var facilityTable = DataLoadManager.Instance.GetFacilityTable();
+            var groundTable = DataLoadManager.Instance.GetGroundTable();
+            var userData = DataLoadManager.Instance.GetUserData();
+            Debug.Log($"Engine Table Count: {engineTable.Count}");
+            Debug.Log($"Facility Table Count: {facilityTable.Count}");
+            Debug.Log($"Ground Table Count: {groundTable.Count}");
+            Debug.Log($"User Data Count: {userData.Count}");
+        }
         #endregion
 
         #region Unity event methods
         private void Awake()
         {
-        
+
         }
 
         private void Start()
         {
-            var engineTable = DataLoadManager.Instance.GetEngineTable();
-            var facilityTable = DataLoadManager.Instance.GetFacilityTable();
-            var groundTable = DataLoadManager.Instance.GetGroundTable();
-            var userData = DataLoadManager.Instance.GetUserData();
-            var groundPrefab = DataLoadManager.Instance.GetGroundPrefab();
-            Debug.Log($"Engine Table Count: {engineTable.Count}");
-            Debug.Log($"Facility Table Count: {facilityTable.Count}");
-            Debug.Log($"Ground Table Count: {groundTable.Count}");
-            Debug.Log($"User Data Count: {userData.Count}");
-            Debug.Log($"Ground Prefab: {groundPrefab.name}");
+            StartCoroutine(DataCheckCoroutine());
         }
 
         private void Update()
