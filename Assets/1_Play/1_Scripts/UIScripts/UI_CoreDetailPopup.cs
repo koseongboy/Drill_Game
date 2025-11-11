bfcfc2246447efef801cf0491e38f188eb6ab099
Crocobob <mjdaniel911@naver.com>
2025-11-11 16:32:00 +0900

    코어 누르면 UI 나옴
    Popup UI 템플릿 만듦.

---
+++ Assets/1_Play/1_Scripts/UIScripts/UI_CoreDetailPopup.cs
@@ -0,0 +1,58 @@
+using DrillGame.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+using DrillGame.UI.Interface;
+
+namespace DrillGame
+{
+    public class UI_CoreDetailPopup : MonoBehaviour, UI_IAddressable
+    {
+        #region Fields & Properties
+
+        [SerializeField]
+        private string addressableName;
+        #endregion
+
+        #region Singleton & initialization
+        #endregion
+
+        #region getters & setters
+        #endregion
+
+        #region public methods
+        public void CloseUI()
+        {
+            Debug.Log($"{gameObject.name}: UI 종료 시도, addressable 주소 : {addressableName}");
+            UILoader.Instance.HideUI(addressableName);
+        }
+
+        public void LinkAddressable(string address)
+        {
+            Debug.Log($"{gameObject.name}: addressable 주소 설정 : {address}");
+            addressableName = address;
+        }
+        #endregion
+
+        #region private methods
+        #endregion
+
+        #region Unity event methods
+        private void Awake()
+        {
+            Debug.Log($"{gameObject.name}: Awake 호출");
+        }
+        #endregion
+
+        private void OnEnable()
+        {
+            Debug.Log("UI_CoreDetailPopup : OnEnable");
+            OpenAction();
+        }
+
+        private void OpenAction()
+        {
+            // 여기서 UI 열릴 때 연출.
+        }
+    }
+}
