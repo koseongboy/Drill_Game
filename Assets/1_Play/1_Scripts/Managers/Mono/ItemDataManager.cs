232f4ba289fefff31e916aeaaddab41b53929ea9
Crocobob911 <mjdaniel911@naver.com>
2025-11-17 13:49:18 +0900

    아. ItemData Getter를 Public으로 수정

--- Assets/1_Play/1_Scripts/Managers/Mono/ItemDataManager.cs
+++ Assets/1_Play/1_Scripts/Managers/Mono/ItemDataManager.cs
@@ -27,6 +27,15 @@ namespace DrillGame
         #endregion
     
         #region getters & setters
+        public ItemData GetItemData(string itemId)
+        {
+            if (itemDataDict.TryGetValue(itemId, out ItemData itemData))
+            {
+                return itemData;
+            }
+            Debug.LogWarning($"경고: 올바르지 않은 ItemId입니다. (string 형식이 맞는지도 확인하세요.) : {itemId}");
+            return null;
+        }
         #endregion
     
         #region public methods
@@ -54,16 +63,6 @@ namespace DrillGame
             
                 itemDataDict.Add(resource.itemId, resource);
             }
-        }
-
-        private ItemData GetItemData(string itemId)
-        {
-            if (itemDataDict.TryGetValue(itemId, out ItemData itemData))
-            {
-                return itemData;
-            }
-            Debug.LogWarning($"경고: 올바르지 않은 ItemId입니다. (string 형식이 맞는지도 확인하세요.) : {itemId}");
-            return null;
         }
         #endregion
     }
