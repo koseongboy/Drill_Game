7ea4c98b4f0e42a4055e1cb3a7980b57fc2be89e
Crocobob911 <mjdaniel911@naver.com>
2025-11-26 15:05:46 +0900

    Core_Data_추가, UI_Research 확장성 추가

---
+++ Assets/1_Play/1_Scripts/UIScripts/FullWindow/UI_ResearchUnitList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DrillGame._1_Play._1_Scripts.UIScripts.FullWindow
+{
+    public class UI_ResearchUnitList : MonoBehaviour
+    {
+        [SerializeField]
+        private int techID;
+        [SerializeField]
+        private List<UI_ResearchUnit> unitList = new List<UI_ResearchUnit>();
+
+        [SerializeField] private string name;
+
+        private void OnEnable()
+        {
+            foreach (var unit in unitList)
+            {
+                unit.SetData(techID, name);
+            }
+        }
+    }
+}
