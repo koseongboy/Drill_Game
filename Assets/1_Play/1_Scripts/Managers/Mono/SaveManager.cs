a38920db49ae6ee437fc36c333ccb6d09aeb6493
simplee <simplee@cau.ac.kr>
2025-12-06 03:40:20 +0900

    엔진 머저 데이터 이관

--- Assets/1_Play/1_Scripts/Managers/Mono/SaveManager.cs
+++ Assets/1_Play/1_Scripts/Managers/Mono/SaveManager.cs
@@ -14,7 +14,7 @@ namespace DrillGame.Managers
         {
             { "RESEARCH_SELECTED_ID_KEY", "ResearchId" },
             { "RESEARCH_PROGRESS_KEY", "ResearchProgressData" },
-
+            { "ENGINE_MERGER_KEY", "EngineMergerData" },
 
         };
 
@@ -88,6 +88,41 @@ namespace DrillGame.Managers
             ES3.DeleteKey(SaveKeys["RESEARCH_PROGRESS_KEY"]);
         }
 
+        [ContextMenu("리서치 매니저 단독 초기화")]
+        public void InitializeResearchManagerData()
+        {
+            ES3.Save(SaveKeys["RESEARCH_SELECTED_ID_KEY"], 1);
+            ES3.Save(SaveKeys["RESEARCH_PROGRESS_KEY"], new Dictionary<int, float>());
+            Debug.Log("리서치 매니저 데이터가 초기화되었습니다.");
+        }
+
+
+        public void SaveEngineMergerData(EngineMergerData engineMergerData)
+        {
+            ES3.Save(SaveKeys["ENGINE_MERGER_KEY"], engineMergerData);
+        }
+        public EngineMergerData LoadEngineMergerData(EngineMergerData defaultValue)
+        {
+            if (ES3.KeyExists(SaveKeys["ENGINE_MERGER_KEY"]))
+            {
+                return ES3.Load<EngineMergerData>(SaveKeys["ENGINE_MERGER_KEY"]);
+            }
+            Debug.LogWarning("No saved EngineMergerData found.");
+            return defaultValue;
+        }
+        public void DeleteEngineMergerData()
+        {
+            ES3.DeleteKey(SaveKeys["ENGINE_MERGER_KEY"]);
+        }
+
+        [ContextMenu("엔진 머저 매니저 단독 초기화")]
+        public void InitializeEngineMergerManagerData()
+        {
+            var defaultData = new EngineMergerData(0, 0, 0, 0);
+            ES3.Save(SaveKeys["ENGINE_MERGER_KEY"], defaultData);
+            Debug.Log("엔진 머저 매니저 데이터가 초기화되었습니다.");
+        }
+
         [ContextMenu("리셋 데이터")]
         public void ResetAllData()
         {
@@ -102,27 +137,33 @@ namespace DrillGame.Managers
         [ContextMenu("첫 실행 데이터 설정")]
         public void SetFirstLaunchData()
         {
-            ES3.Save(IS_FIRST_LAUNCH_KEY, false);
+            ES3.Save(IS_FIRST_LAUNCH_KEY, 1);
             ES3.Save(SaveKeys["RESEARCH_SELECTED_ID_KEY"], 1);
             ES3.Save(SaveKeys["RESEARCH_PROGRESS_KEY"], new Dictionary<int, float>());
+
+            ES3.Save(SaveKeys["ENGINE_MERGER_KEY"], new EngineMergerData(0, 0, 0, 0));
 
 
             Debug.Log("첫 실행 데이터가 설정되었습니다.");
         }
 
-        [ContextMenu("리서치 매니저 단독 초기화")]
-        public void InitializeResearchManagerData()
-        {
-            ES3.Save(SaveKeys["RESEARCH_SELECTED_ID_KEY"], 1);
-            ES3.Save(SaveKeys["RESEARCH_PROGRESS_KEY"], new Dictionary<int, float>());
-            Debug.Log("리서치 매니저 데이터가 초기화되었습니다.");
-        }
 
 
 
         #endregion
 
         #region private methods
+        private bool IsFirstLaunch()
+        {
+            if (ES3.KeyExists(IS_FIRST_LAUNCH_KEY))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
         #endregion
 
         #region Unity event methods
